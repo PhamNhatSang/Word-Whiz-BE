d1bdfd6a40e431bc18b0a682053174a23426d247
PhamNhatSang <sang.pn194657@sis.hust.edu.vn>
2024-03-17 15:56:44 +0700

    update dependencies v5

--- dist/database.js
+++ dist/database.js
@@ -1,28 +1,37 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createDatabase = void 0;
 const sequelize_typescript_1 = require("sequelize-typescript");
 const book_model_1 = require("./models/book.model");
 const cloud_sql_connector_1 = require("@google-cloud/cloud-sql-connector");
-async function createDatabase({ instanceConnectionName, username, databaseName, }) {
-    const connector = new cloud_sql_connector_1.Connector();
-    const clientOpts = await connector.getOptions({
-        instanceConnectionName,
-        ipType: cloud_sql_connector_1.IpAddressTypes.PUBLIC,
-        //authType: AuthTypes.IAM,
+function createDatabase(_a) {
+    return __awaiter(this, arguments, void 0, function* ({ instanceConnectionName, username, databaseName, }) {
+        const connector = new cloud_sql_connector_1.Connector();
+        const clientOpts = yield connector.getOptions({
+            instanceConnectionName,
+            ipType: cloud_sql_connector_1.IpAddressTypes.PUBLIC,
+            //authType: AuthTypes.IAM,
+        });
+        const database = new sequelize_typescript_1.Sequelize({
+            dialect: "mysql",
+            username,
+            database: databaseName,
+            models: [book_model_1.Book],
+            dialectOptions: Object.assign({}, clientOpts),
+        });
+        yield database.authenticate();
+        yield database.sync({ force: true });
+        return database;
     });
-    const database = new sequelize_typescript_1.Sequelize({
-        dialect: "mysql",
-        username,
-        database: databaseName,
-        models: [book_model_1.Book],
-        dialectOptions: {
-            ...clientOpts,
-        },
-    });
-    await database.authenticate();
-    await database.sync({ force: true });
-    return database;
 }
 exports.createDatabase = createDatabase;
 //# sourceMappingURL=database.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -12,18 +21,20 @@ const database_1 = require("./database");
 const instanceConnectionName = process.env.CLOUD_INSTANCE || '';
 const username = process.env.DB_USERNAME || '';
 const databaseName = process.env.DB_NAME || '';
-async function startServer() {
-    const database = await (0, database_1.createDatabase)({ instanceConnectionName, username, databaseName });
-    console.log(database);
-    app.get('/', (_req, res) => {
-        console.log('Request received');
-        return res.send('Request received');
-    });
-    app.get('/ping', (_req, res) => {
-        return res.send('pong 🏓');
-    });
-    app.listen(port, () => {
-        return console.log(`Server is listening on ${port}`);
+function startServer() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const database = yield (0, database_1.createDatabase)({ instanceConnectionName, username, databaseName });
+        console.log(database);
+        app.get('/', (_req, res) => {
+            console.log('Request received');
+            return res.send('Request received');
+        });
+        app.get('/ping', (_req, res) => {
+            return res.send('pong 🏓');
+        });
+        app.listen(port, () => {
+            return console.log(`Server is listening on ${port}`);
+        });
     });
 }
 startServer().then(() => {
