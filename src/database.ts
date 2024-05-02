11022995187a1cad80ec50147985b6c85eb06b9f
PhamNhatSang <sang.pn194657@sis.hust.edu.vn>
2024-05-02 21:12:43 +0700

    Update railway

--- src/database.ts
+++ src/database.ts
@@ -6,4 +6,4 @@ import 'dotenv/config';
 
 import { dbConfig } from "./config/db.config";
 import { DataSource } from "typeorm";
-export const database = new DataSource({...dbConfig, entities: [__dirname + "/models/**/*.model{.ts,.js}"],migrations: [__dirname + "/migrations/**/*{.ts,.js}"]});
+export const database = new DataSource({...dbConfig, entities: [__dirname+"/models/**/*.model{.ts,.js}"],migrations: ["migrations/**/*{.ts,.js}"]});
