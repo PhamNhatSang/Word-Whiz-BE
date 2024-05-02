3a9624fba1b6042c0e4390ad485ef8811299cb11
PhamNhatSang <sang.pn194657@sis.hust.edu.vn>
2024-05-02 18:22:42 +0700

    update path entites 5

--- dist/database.js
+++ dist/database.js
@@ -5,5 +5,5 @@ require("reflect-metadata");
 require("dotenv/config");
 const db_config_1 = require("./config/db.config");
 const typeorm_1 = require("typeorm");
-exports.database = new typeorm_1.DataSource(Object.assign(Object.assign({}, db_config_1.dbConfig), { entities: [__dirname + "/models/*.model.{ts,js}"], migrations: [__dirname + "/migrations/*.{ts,js}"] }));
+exports.database = new typeorm_1.DataSource(Object.assign(Object.assign({}, db_config_1.dbConfig), { entities: [__dirname + "/models/**/*.model{.ts,.js}"], migrations: [__dirname + "/migrations/**/*{.ts,.js}"] }));
 //# sourceMappingURL=database.js.map
